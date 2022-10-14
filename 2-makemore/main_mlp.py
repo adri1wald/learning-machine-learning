ee6cd2b829c8619192e6b0601f8f401220d61ed7
Adrien Wald <adrien@genei.io>
2022-10-14 17:29:03 +0200

    feat: mlp parameters, forward pass, loss calc

--- 2-makemore/main_mlp.py
+++ 2-makemore/main_mlp.py
@@ -1,5 +1,5 @@
-from venv import create
 import torch 
+import torch.nn.functional as F
 from tokenizer import Tokenizer
 
 def init_gen():
@@ -34,18 +34,36 @@ def main():
     VOCABULARY = Tokenizer.compute_vocabulary(WORDS)
     START_TOKEN = '.'
     CONTEXT_SIZE = 3
+    EMBEDDING_DIM = 2
+    L1_SIZE = 100
     tokenizer = Tokenizer(
         vocabulary=VOCABULARY,
         start_token=START_TOKEN
     )
 
     words = WORDS[:5]
-    xs, ys = create_dataset(words, tokenizer, CONTEXT_SIZE)
-    print(xs.shape)
-    print(ys.shape)
+    X, Y = create_dataset(words, tokenizer, CONTEXT_SIZE)
     
+    # The embedding matrix
+    C = torch.randn((tokenizer.vocab_size, EMBEDDING_DIM), generator=g)
+    # Layer 1
+    W1 = torch.randn((CONTEXT_SIZE * EMBEDDING_DIM, L1_SIZE), generator=g)
+    b1 = torch.randn(L1_SIZE, generator=g)
+    # Layer 2
+    W2 = torch.randn((L1_SIZE, tokenizer.vocab_size), generator=g)
+    b2 = torch.randn(tokenizer.vocab_size, generator=g)
 
+    # forward pass
+    embs = C[X]
+    h = (embs.view(-1, CONTEXT_SIZE * EMBEDDING_DIM) @ W1 + b1).tanh()
+    logits = h @ W2 + b2
 
+    # loss calc
+    counts = logits.exp()
+    probs = counts / counts.sum(1, keepdim=True)
+    loss = -probs[torch.arange(Y.nelement()), Y].log().mean()
+    print(loss)
+    
 
 if __name__ == '__main__':
     main()
